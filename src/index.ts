27bc970b2b78ee793e288af395b954c4659bb0e8
andrewvasilchuk <andrew.d.vasilchuk@gmail.com>
2020-08-28 17:19:00 +0300

    refactor: do not provide default generic argument

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ import Vue, { VueConstructor } from 'vue'
 
 export type WithRefs<
   R extends Record<string, Vue | Element | Vue[] | Element[] | undefined>,
-  C extends VueConstructor<any> = VueConstructor<Vue>
+  C extends VueConstructor = VueConstructor
 > = C extends VueConstructor<infer V> ? VueConstructor<V & { $refs: R }> : never
